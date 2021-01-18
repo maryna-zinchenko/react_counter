aa8329ac5be8a2b7ddeaea2a68157cd775646ed7
Maryna Zinchenko <pandoz16@gmail.com>
2021-01-18 16:58:32 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.0ff14ff1e129d6bde2efea0dd8fb4d56.js"
 );
 
 self.addEventListener('message', (event) => {
